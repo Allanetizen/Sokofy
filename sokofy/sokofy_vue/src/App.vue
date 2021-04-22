1b7086cffad53355b7d5e1b3b3347a3e84be2cc7
Allanetizen <allankosgei747@gmail.com>
2021-04-22 23:03:16 +0300

    vue dashboard

--- sokofy/sokofy_vue/src/App.vue
+++ sokofy/sokofy_vue/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class = "navbar-brand>
+        <router-link to="/" class="navbar-item"><strong>Sokofy</strong></router-link>
+   
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          </a>
+          </div> 
+         <div class="navbar-menu" id="navbar-menu">
+           <div class="navbar-end">
+             <router-link to="/summer" class="navbar-item">summer</router-link>
+              <router-link to="/winter" class="navbar-item">winter</router-link>
+              <div class="navbar-item">
+                <div class="buttons">
+                  <router-link to="/log-in" class="button is-light">Log in </router-link>
+                  <
+                </div>
+              </div>
+
+
+           </div>
+         </div>
+    </nav>
+    
   <router-view/>
+  </div>
+ 
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '../node_modules/bulma';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+ 
